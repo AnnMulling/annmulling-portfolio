5c783ff21adaee81ddd6ca8b4252c55e27d27692
AnnPrim <Primpraowann@gmail.com>
2024-03-29 13:20:00 -0500

    project content block in progress

--- src/components/ProjectPage/Project.jsx
+++ src/components/ProjectPage/Project.jsx
@@ -6,6 +6,8 @@ import SocialTag from "../Navbar/SocialTag";
 //icons
 import { CgMenuGridR } from "react-icons/cg";
 import { TfiLayoutMenuV } from "react-icons/tfi";
+import { FaGithub } from "react-icons/fa";
+import { MdWeb } from "react-icons/md";
 
 //css & other
 import '../../style/components/workPage.css';
@@ -60,23 +62,31 @@ export default function Work() {
 
                 {listView &&
                     <div className="list-view">
-                        <div>
+                        <div className="list-view-titles">
                             <span>Project Name</span>
                              <span>Tools</span>
                              <span>Code & Site</span>
                         </div>
+
                         {projects.map((project) => (
+                            <ul key={project.id}>
+                                <li>{project.name}
+                                    <div className="project-content">
+                                        {project.techStack.map((tool) => (
+                                            <span className="tools-block" key={tool.id}><tool.name/></span>
+                                            ))}
+                                    </div>
 
-                            <ul id={project.id}>
-                                <li>
+                                    <div className="project-content">
+                                        {project.url.map((url) => (
+                                            <span className="url-block" key={url.id}>
+                                                <a href={url.url} target="_blank"><span><url.icon/></span></a>
+                                            </span>
+                                        ))}
 
+                                    </div>
                                 </li>
-                                <li>{project.name}</li>
-                                <li>
-                                    {project.techStack.map((tool) => {
-                                         return <span>{<tool/>}</span>
-                                    })}
-                                </li>
+
 
                             </ul>
                         ))}
