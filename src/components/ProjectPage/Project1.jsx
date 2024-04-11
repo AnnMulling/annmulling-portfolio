e698658eebfadefe60b436f522578f29db58ba04
AnnPrim <Primpraowann@gmail.com>
2024-04-10 22:42:34 -0500

    working on carousel projects

--- src/components/ProjectPage/Project1.jsx
+++ src/components/ProjectPage/Project1.jsx
@@ -2,9 +2,35 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 //css & others
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+
 
 export default function Project1 () {
     return (
-        <>Test Carousel</>
+        <>
+            <Swiper
+                cssMode={true}
+                navigation={true}
+                pagination={true}
+                mousewheel={true}
+                keyboard={true}
+                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                className="mySwiper"
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
+    </>
     )
 }
