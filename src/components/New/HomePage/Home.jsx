4677b2870ea170c9f533f4a61011d73dd846f5a3
AnnPrim <Primpraowann@gmail.com>
2024-09-19 15:40:39 -0500

    started new style on test route

--- src/components/New/HomePage/Home.jsx
+++ src/components/New/HomePage/Home.jsx
@@ -5,7 +5,7 @@ import "../../../style/components/New/homePage.css"
 export default function HomePage() {
     return (
         <>
-        <div>
+        <div className="home-hero">
             <h1>Hello new style</h1>
         </div>
         </>
