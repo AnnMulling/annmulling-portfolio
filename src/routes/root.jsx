f7520be12d28f6910c651be7ad802020c3dff37b
AnnPrim <Primpraowann@gmail.com>
2024-09-25 22:19:44 -0500

    completed slide 2

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,26 +1,50 @@
 import { Outlet } from "react-router-dom";
 import  Nav from '../components/Navbar/Nav';
 import  MainPage  from "../components/HomePage/Main";
+import AboutMe from '../components/AboutPage/About.jsx';
 import { Link } from "react-router-dom";
 
+
+//css & other
+import { Swiper, SwiperSlide } from 'swiper/react';
 import '../style/Main.css'
+import 'swiper/scss';
+import 'swiper/scss/pagination';
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 export default function Root() {
     return (
       <>
-        <Link to="/" className="link">
-          <div className="initial-home-btn">
-            <span>AM</span>
-          </div>
-        </Link>
         <div className="aside-left">
-            <h1> A Junior Software Engineer</h1>
+          <h1> A Junior Software Engineer</h1>
         </div>
-        <div className="line-left"> </div>
-        
-        <Nav />
-        <MainPage />
+
+        <div className="initial-home-btn">
+              <Link to="/" className="link">
+                  <span>AM</span>
+              </Link>
+        </div>
+        <div className="line-left">
+
+        </div>
+        <Swiper
+          pagination={{
+            dynamicBullets: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+
+          <SwiperSlide> <MainPage /> </SwiperSlide>
+          <SwiperSlide> <AboutMe /> </SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+        </Swiper>
+
+
+
         <div className="line-bt"></div>
       </>
+
     );
   }
