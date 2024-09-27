41143a137e376fdc10fb5207c39da871947c11a8
AnnPrim <Primpraowann@gmail.com>
2024-09-26 22:25:43 -0500

    shorten css

--- src/components/HomePage/Main.jsx
+++ src/components/HomePage/Main.jsx
@@ -1,23 +1,28 @@
 
 import SocialTag from '../Navbar/SocialTag';
-import  Nav from '../Navbar/Nav';
+import Nav from '../Navbar/Nav';
 //css & other
 import '../../style/components/mainPage.css';
 
 export default function MainPage() {
 
-    return(
-        <>
-
-           <div className="main-page">
-                <div className="main-page-info">
-                        <h1>Ann Mulling</h1>
+    return (
+            <>
+                <div className="aside-left">
+                    <h1> A Junior Software Engineer</h1>
                 </div>
+                <div className="main-page">
+                    {/* <div className="line-left"></div> */}
+                    {/* <div className="main-page-info"> */}
+                        <h1>Ann Mulling</h1>
+                    {/* </div> */}
 
-           </div>
-           
-           <SocialTag />
-        </>
+                    {/* <div className="line-bt"></div> */}
+                </div>
+            </>
+
+
+        //    <SocialTag />
 
     )
 }
