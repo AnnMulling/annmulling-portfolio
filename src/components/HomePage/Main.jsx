a01e3e24eaabc1e44d784e9939454bd2f4e3d1d4
AnnPrim <Primpraowann@gmail.com>
2024-10-01 18:57:01 -0500

    change the layout to grid row

--- src/components/HomePage/Main.jsx
+++ src/components/HomePage/Main.jsx
@@ -7,22 +7,19 @@ import '../../style/components/mainPage.css';
 export default function MainPage() {
 
     return (
-            <>
+
+            <div className="main-page">
                 <div className="aside-left">
                     <h1> A Junior Software Engineer</h1>
+                    <div className="bottom-line"></div>
                 </div>
-                <div className="main-page">
-                    {/* <div className="line-left"></div> */}
-                    {/* <div className="main-page-info"> */}
-                        <h1>Ann Mulling</h1>
-                    {/* </div> */}
+                <div className="main-page-info">
+                    <h1>Ann Mulling</h1>
 
-                    {/* <div className="line-bt"></div> */}
                 </div>
-            </>
+                {/* <SocialTag /> */}
+            </div>
 
-
-        //    <SocialTag />
 
     )
 }
